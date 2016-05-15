77b59ab3262b36ddb8bcfc8624cc05749db263aa
Adriel Oliveira <richardd.oci12@gmail.com>
2016-05-15 14:12:41 -0300

    Ad All Set

--- Assets/Resources/Scripts/Ads.cs
+++ Assets/Resources/Scripts/Ads.cs
@@ -1,16 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Advertisements;
+using UnityEngine.UI;
 
 public class Ads : MonoBehaviour {
 
     [SerializeField] string IDAdds;
     BonusAdLayout b = new BonusAdLayout ();
 
+    Animator rand;
+    Animator bonus;
+    GameObject g;
+    GameObject gR;
+    public Sprite[] s;
+    
     void Awake () {
         b.Initialize ();
         Advertisement.Initialize (IDAdds, true);
 	}
+
+    void Start () {
+        gR = GameObject.Find ("Canvas/GameOverHUD/BONUSLAYOUT/Bonus");
+        g = GameObject.Find ("Canvas/GameOverHUD/BONUSLAYOUT/BonusPowerUp");
+        rand = g.GetComponent<Animator> ();
+        bonus = gR.GetComponent<Animator> ();
+        rand.enabled = true;
+        bonus.enabled = true;
+        rand.SetBool ("canGo", false);
+        bonus.SetBool ("GoOn", false);
+    }
 	
     public void ShowAd () {
 
@@ -24,19 +42,36 @@ public class Ads : MonoBehaviour {
 
     void AdCallbackHandler (ShowResult result) {
 
+        b.CallBonus ();
+        bonus.SetBool ("GoOn", true);
+        rand.SetBool ("canGo", true);
+
         switch (result) {
             case ShowResult.Finished:
                 int a =  Random.Range (0, 3);
-            if (a == 0)
-                SetNuts.quantHammer++;
-            else if (a == 1)
-                SetNuts.quantClock++;
-            else if (a == 2)
-                SetNuts.quantSpdd++;
+                if (a == 0) {
+                    StartCoroutine (WaitAnimation (s[0]));
+                    SetNuts.quantHammer++;
+                }
+                else if (a == 1) {
+                    StartCoroutine (WaitAnimation (s[1]));
+                    SetNuts.quantClock++;
+                }
+                else if (a == 2) {
+                    StartCoroutine (WaitAnimation (s[2]));
+                    SetNuts.quantSpdd++;
+                }
 
-            b.CallBonus ();
             break;
 
         }
+    }
+
+
+    IEnumerator WaitAnimation (Sprite s) {
+        yield return new WaitForSeconds (1f);
+        rand.enabled = false;
+        g.gameObject.GetComponent<Image> ().sprite = s;
+
     }
 }
